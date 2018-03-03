d56dc3bb623040028bedfe798f94047e3e784e4d
sanghun.an <anbv3@naver.com>
2018-03-03 14:00:25 +0900

    Upgrade to Boot 2.0 GA

--- src/main/java/spring/sample/webflux/WebfluxTemplateApplication.java
+++ src/main/java/spring/sample/webflux/WebfluxTemplateApplication.java
@@ -1,6 +1,7 @@
 package spring.sample.webflux;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
@@ -9,6 +10,8 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 public class WebfluxTemplateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebfluxTemplateApplication.class, args);
+		SpringApplication app = new SpringApplication(WebfluxTemplateApplication.class);
+		app.setWebApplicationType(WebApplicationType.REACTIVE);
+        app.run(args);
 	}
 }
--- src/main/java/spring/sample/webflux/controllers/MyController.java
+++ src/main/java/spring/sample/webflux/controllers/MyController.java
@@ -1,19 +1,25 @@
 package spring.sample.webflux.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
 @RestController
 public class MyController {
+    private static final Logger LOG = LoggerFactory.getLogger(MyController.class);
 
     // [NOTE] https://github.com/simonbasle-demos/reactor-by-example
-
     @GetMapping("controller/echo/{echo}")
-    public Mono<String> echo(@PathVariable String echo) {
+    public Mono<String> echo(@PathVariable String echo, ServerHttpRequest request) {
+        LOG.info("client URI: {}", request.getURI());
         return Mono.just(echo)
                    .map(e -> e);
     }
@@ -24,4 +30,12 @@ public class MyController {
                    .delaySubscription(Duration.ofSeconds(3));
     }
 
+    @GetMapping("controller/webclient")
+    public Mono<String> testWebClient() {
+        return WebClient.create("https://api.github.com/")
+                        .get()
+                        .accept(MediaType.APPLICATION_JSON)
+                        .retrieve()
+                        .bodyToMono(String.class);
+    }
 }
